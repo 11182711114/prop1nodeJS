dbdad895e0759b94acf6b2fb9ecb301ae9da0a09
Fredrik Larsson <flarsson93@gmail.com>
2017-11-23 13:58:06 +0100

    fix create logic

--- prop_assign1.js
+++ prop_assign1.js
@@ -3,7 +3,7 @@
 // from being saved in memory for every object created by myObject
 var myObject = {
     create: function(prototypeList) {
-        let newObj = {};
+        var newObj = {};
         // Necessary? This is bad since these functions will now be stored in memory for all instances of created objects
         // Alternative? Chuck them into __proto__, since we are overriding protos is this reliable? 
         //      Should be since whatever chain you make it will end with the original __proto__
@@ -15,17 +15,22 @@ var myObject = {
         let currentPrototypeLevel = newObj;
         //fix naming...
         prototypeList.forEach(function(prototype, index, array) {
-            // if the object already has prototypes that is not the original(i.e. myObject.__proto__) 
-            if (currentPrototypeLevel.__proto__ != this.__proto__) {
-                //iterate through the prototypes and add the them at the top of the chain to give them "lowest priority" lookup
-                for (let proto = currentPrototypeLevel.__proto__; proto.__proto__.__proto__ != null; proto = proto.__proto__) {
-                    currentPrototypeLevel = proto;
+            if (prototype != null) {
+                // if the object already has prototypes that is not the original(i.e. myObject.__proto__) 
+                if (currentPrototypeLevel.__proto__ != this.__proto__) { // TODO: doesnt work properly
+                    //iterate through the prototypes and add the them at the top of the chain to give them "lowest priority" lookup
+                    // note: stops on the object before default __proto__ (i.e. proto.__proto__.__proto__ != null)
+                    for (let proto = currentPrototypeLevel.__proto__; proto.__proto__ != null; proto = proto.__proto__) {
+                        currentPrototypeLevel = proto;
+                    }
+                    currentPrototypeLevel.__proto__ = prototype;
+                    currentPrototypeLevel = prototype;
+                // if the object does not have other prototypes just add prototype from prototypeList.forEach as __proto__ 
+                // and set currentPrototypeLevel to prototype to advance in the chain
+                } else {
+                    currentPrototypeLevel.__proto__ = prototype;
+                    currentPrototypeLevel = prototype;
                 }
-            // if the object does not have other prototypes just add prototype from prototypeList.forEach as __proto__ 
-            // and set currentPrototypeLevel to prototype to advance in the chain
-            } else {
-                currentPrototypeLevel.__proto__ = prototype;
-                currentPrototypeLevel = prototype;
             }
         }, this);
         return newObj;
@@ -60,9 +65,17 @@ var myObject = {
 
 
 /*
-*******************************************
-*              TESTS                      *
-*******************************************
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+¤%%%%%%%%%%%%%%%%%%%%%%%¤
+¤%@@@@@@@@@@@@@@@@@@@@@%¤
+¤%@###################@%¤
+¤%@#*****************#@%¤
+¤%@#*     TESTS     *#@%¤
+¤%@#*****************#@%¤
+¤%@###################@%¤
+¤%@@@@@@@@@@@@@@@@@@@@@%¤
+¤%%%%%%%%%%%%%%%%%%%%%%%¤
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
 */
 
 /*
@@ -99,15 +112,35 @@ result = obj0.call("func", ["hello"]);
 console.log("should print ’func0: hello’ ->", result);
 
 /*
+* Check adds all correct 
+*/
+obj0 = myObject.create(null);
+obj0.ident = 0;
+obj1 = myObject.create([obj0]);
+obj1.ident = 1;
+obj2 = myObject.create([null]);
+obj2.ident = 2;
+obj3 = myObject.create([obj1,obj2]);
+obj3.ident = 3;
+
+result = "should print: ’3 1 0 2’ ->";
+for (let obj = obj3; obj.__proto__ != null; obj = obj.__proto__) {
+    result += " " + obj.ident;
+}
+console.log(result);
+ 
+
+/*
 * Preventing circular inheritance
 */
 obj0 = myObject.create(null);
 obj1 = myObject.create([obj0]);
+console.log("should print 'Detected circular logic' x2")
 try {
     obj0.addPrototype(obj1);
     console.log("Failed to detect circular inheritence: 1");
 } catch (error) {
-    console.log("Detected circular logic"); // Should be printed as "Error: Prototype exists in the inheritance chain"
+    console.log("\tDetected circular logic: 1"); // Should be printed as "Error: Prototype exists in the inheritance chain"
 }
 
 /*
@@ -121,12 +154,14 @@ try {
     obj0.addPrototype(obj3);
     console.log("Failed to detect circular inheritence: 2");
 } catch (error) {
-    console.log("Detected circular logic"); // Should be printed as "Error: Prototype exists in the inheritance chain"
+    console.log("\tDetected circular logic: 2"); // Should be printed as "Error: Prototype exists in the inheritance chain"
 }
+
 
 /*
 * Middle circular inheritence
 */
+/*
 obj0 = myObject.create(null);
 obj1 = myObject.create([obj0]);
 obj2 = myObject.create([obj1]); 
@@ -135,5 +170,6 @@ try {
     obj0.addPrototype(obj3);
     console.log("Failed to detect circular inheritence: 3");
 } catch (error) {
-    console.log("Detected circular logic"); // Should be printed as "Error: Prototype exists in the inheritance chain"
+    console.log("\tDetected circular logic"); // Should be printed as "Error: Prototype exists in the inheritance chain"
 }
+*/
