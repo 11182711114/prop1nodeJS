93dde89fbec2425ef0403ee37dac781fd9fc161d
unknown <flarsson93@gmail.com>
2017-11-29 00:14:38 +0100

    add part two

---
+++ part_two.js
@@ -0,0 +1,71 @@
+
+var superDuperClass = {
+    call : function() {
+
+    }
+};
+
+var createClass = function(className, superClassList) {
+    let newClass = { 
+        name:className, 
+        new:function(){
+            return this.copy();
+        }
+    };
+
+    return newClass;
+};
+
+
+var newClass = createClass("Test", null);
+console.log("new class: " + newClass.name);
+var newObj = newClass.new();
+console.log("new class instance: " + newObj.name);
+
+
+
+
+
+// /*
+//             %%%%%%%%%%%%%%%%%%%%%%%
+//             %@@@@@@@@@@@@@@@@@@@@@%
+//             %@###################@%
+//             %@#*****************#@%
+//             %@#*     TESTS     *#@%
+//             %@#*****************#@%
+//             %@###################@%
+//             %@@@@@@@@@@@@@@@@@@@@@%
+//             %%%%%%%%%%%%%%%%%%%%%%%
+// */
+
+// /*
+// *   Example
+// */
+// var class0 = createClass("Class0", null);
+// class0.func = function(arg) { return "func0: " + arg; };
+// var class1 = createClass("Class1", [class0]);
+// var class2 = createClass("Class2", []);
+// class2.func = function(arg) { return "func2: " + arg; };
+// var class3 = createClass("Class3", [class1, class2]);
+// var obj3 = class3.new();
+// var result = obj3.call("func", ["hello"]);
+
+// /*
+// *   Another example of method lookup testing
+// */
+// class0 = createClass("Class0", null);
+// class0.func = function(arg) { return "func0: " + arg; };
+// class1 = createClass("Class1", [class0]);
+// class2 = createClass("Class2", []);
+// class3 = createClass("Class3", [class2, class1]);
+// obj3 = class3.new();
+// result = obj3.call("func", ["hello"]);
+
+// /*
+// *   Another example of method lookup testing that the method 
+// *   will be found in the object’s own class:
+// */
+// class0 = createClass("Class0", null);
+// class0.func = function(arg) { return "func0: " + arg; };
+// var obj0 = class0.new();
+// result = obj0.call("func", ["hello"]);
