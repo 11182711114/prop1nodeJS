850d4ed3bc3b14072910161b75722619afb3b220
Fredrik Larsson <flarsson93@gmail.com>
2017-12-01 13:15:57 +0100

    comments

--- part_two.js
+++ part_two.js
@@ -1,31 +1,23 @@
 // Fredrik Larsson frla9839, Louise Flinta loflXXXX
 
 var superDuperClass = {
+    //  call handles call functionality within the instances of objects and hands over the class functionality to callInClass
     call: function(funcName, param) {
         if (this.hasOwnProperty(funcName))
             return this[funcName].apply(this, param);
 
-        // Fix return? -> tail recursion should not be necessary since the return value
-        // is not dependent on the previous stack windows(except branch points, i.e. classes with multiple super-classes)
-        // which means that they should not be saved on the stack?
+        //  Fix return? -> tail recursion should not be necessary since the return value
+        //      is not dependent on the previous stack windows(except branch points, i.e. classes with multiple super-classes)
+        //          which means that they should not be saved on the stack?
         let result = this.__proto__.callInClass(funcName, param);
         return result;
     },
+    //  Handles the call function within classes
     callInClass: function(funcName, param) {
         if (this.hasOwnProperty(funcName)) {
            return this[funcName].apply(this, param);
         } else {
-            //      Cannot break out of forEach since we are breaking the function which the forEach is calling
-            //      and not the forEach itself
-            //      same applies to return, it simply returns for the function given to forEach
-            /* 
-            this.superClasses.forEach(function(superClass) {
-                result = superClass.callTail(funcName, param, result);
-                if (result != null)
-                    return result;
-            }, this); 
-            */
-            for(let i = 0; i < this.superClasses.length; i++) {
+            for (let i = 0; i < this.superClasses.length; i++) {
                 let superClass = this.superClasses[i];
                 result = superClass.callInClass(funcName, param);
                 if (result != null)
